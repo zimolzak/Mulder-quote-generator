1f966e5006610d4dd8773078196f75bd315a5e2e
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-11-13 09:24:53 -0500

    working clause and partly random multi_clause

--- mulder.pl
+++ mulder.pl
@@ -3,23 +3,38 @@ use strict;
 
 my @noun = split(", ", "the truth, men in power, lies, extraterrestrials");
 my @linking = split(", ", "is, has become, will be, is revealed to be");
+my @conj = split(", ", "and, but, while, if, so, whereas, even if, even though, although, only if, whenever, anywhere that");
 
 
 
 my $i;
 while ($i++ < 100){
 
-my @sentence;
-
-push @sentence, pick_ary(\@noun);
-push @sentence, pick_ary(\@linking);
-push @sentence, pick_ary(\@noun);
-print join(" ", @sentence) . ".\n";
-
+print join("", multi_clause(\@noun, \@linking)) . ".\n";
 
 }
 
+sub multi_clause {
+    my $nr = shift;
+    my $lr = shift;
+    my $n = pick_a_num(1,3);
+    my @sentence;
+    for(1..$n){
+	push @sentence, clause($nr, $lr);
+	push @sentence, ", ", "but", " " if $_ < $n;
+    }
+    return @sentence;
+}
 
+sub clause {
+    my @noun = @{shift()};
+    my @linking = @{shift()};
+    my @sentence;
+    push @sentence, pick_ary(\@noun), " ";
+    push @sentence, pick_ary(\@linking), " ";
+    push @sentence, pick_ary(\@noun);
+    return @sentence;
+}
 
 sub pick_ary {
     my @a = @{shift()};
