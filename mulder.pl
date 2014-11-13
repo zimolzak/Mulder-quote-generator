301455580a6007d6eb2f4e7ab41428437f6c9070
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-11-13 09:35:04 -0500

    upcases correctly

--- mulder.pl
+++ mulder.pl
@@ -5,16 +5,14 @@ my @noun = split(", ", "the truth, a man in power, a lie, an extraterrestrial");
 my @linking = split(", ", "is, has become, will be, is revealed to be");
 my @conj = split(", ", "and, but, while, if, so, whereas, even if, even though, although, only if, whenever, anywhere that");
 
-
-
-my $i;
-while ($i++ < 100){
-
-print join("", multi_clause(\@noun, \@conj, \@linking)) . ".\n";
-
+for (1..100){
+    $_ = join("", multi_clause(\@noun, \@conj, \@linking)) . ".\n";
+    s/^(.)(.*)/\U$1\E$2/;
+    print;
 }
 
 sub multi_clause {
+    # noun ref, conj ref, link ref = ary
     my $nr = shift;
     my @conj = @{shift()};
     my $lr = shift;
@@ -28,6 +26,7 @@ sub multi_clause {
 }
 
 sub clause {
+    # noun ref, link ref = ary
     my @noun = @{shift()};
     my @linking = @{shift()};
     my @sentence;
@@ -38,11 +37,13 @@ sub clause {
 }
 
 sub pick_ary {
+    # ary ref = element
     my @a = @{shift()};
     return $a[pick_a_num(0,$#a)];
 }
 
 sub pick_a_num {
+    # from, to = int
     my $from = shift;
     my $to = shift;
     return int(rand($to + 1 - $from)) + $from;
