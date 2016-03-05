2c10031bc673ccf8c7e01eb61538e5d1914ee720
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-03-04 22:45:10 -0500

    Gut most of the class.

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python2
-# A Twitter Bot for the Raspberry Pi that retweets any content from
 import sys
 import time
 from datetime import datetime 
@@ -7,40 +6,15 @@ from twython import Twython
 
 class bot:
     def __init__(self, c_key, c_secret, a_token, a_token_secret):
-        # Create a Twython API instance
         self.api = Twython(c_key, c_secret, a_token, a_token_secret)
-        # Make sure we are authenticated correctly
         try: 
             self.api.verify_credentials()
         except:
             sys.exit("Authentication Failed")
         self.last_ran = datetime.now() 
 
-    @staticmethod
-    def timestr_to_datetime(timestr):
-        # Convert a string like Sat Nov 09 09:29:55 +0000
-        # 2013 to a datetime object. Get rid of the timezone 
-        # and make the year the current one
-        timestr = "{0} {1}".format(timestr[:19], datetime.now().year)
-        # We now have Sat Nov 09 09:29:55 2013
-        return datetime.strptime(timestr, '%a %b %d %H:%M:%S %Y')
-
-    def retweet_task(self, screen_name):
-        # Retweets any tweets we've not seen 
-        # from a user
-        print "Checking for new tweets from @{0}".format(screen_name)
-        # Get a list of the users latest tweets
-        timeline = self.api.get_user_timeline (screen_name = screen_name)
-        # Loop through each tweet and check if it was 
-        # posted since we were last called
-        for t in timeline:
-            tweet_time = bot.timestr_to_datetime (t['created_at'])
-            if tweet_time > self.last_ran:
-                print "Retweeting {0}".format(t['id']) 
-                # self.api.retweet(id = t['id']) # FIXME uncomment later
-
     def tweet_hardcode_msg(self):
-        self.api.update_status(status='Our darkest secret will come full circle to all that they have to hide. #thexfiles')
+        self.api.update_status(status='A lie will be confronted by someone who reveals the truth. #thexfiles')
 
 if __name__ == "__main__":
 
