71c2c752538276fcaf24cce49076ed233e0dd64c
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-03-05 08:51:37 -0500

    Practice raising, catching exception, doing nothing with it.

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import sys
 import time
 import random
 from datetime import datetime, timedelta
-from twython import Twython
+from twython import Twython, TwythonError
 from mulder import generate_clean_sentence
 
 class bot:
@@ -14,10 +14,17 @@ class bot:
         except:
             sys.exit("Authentication Failed")
 
+    def autofail(self, status):
+        raise TwythonError('Boo')
+
     def tweet_generated_msg(self):
         sentence = generate_clean_sentence(True) # True means Twitter
         print sentence
-        self.api.update_status(status = sentence)
+        try:
+            #self.api.update_status(status = sentence)
+            self.autofail(sentence)
+        except TwythonError as err_msg:
+            print "FAIL\n    " + str(err_msg) + "\n    Not retrying."
 
 if __name__ == "__main__":
     keys_tokens = open('keys_tokens.txt', 'r').read().splitlines()
@@ -37,5 +44,7 @@ if __name__ == "__main__":
         twitter.tweet_generated_msg()
         avg_hours = 1.0
         sleep_sec = random.expovariate(1.0 / avg_hours) * 60 * 60
-        print "Done. Next tweet approx", datetime.now() + timedelta(seconds=sleep_sec)
+        print "Done.", 
+        print ("Next tweet approx " +
+               str(datetime.now() + timedelta(seconds=sleep_sec)))
         time.sleep(sleep_sec)
