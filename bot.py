4f2ee42dc8c8fc3b7594f6d46c2f3fdfc19c3081
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-03-05 09:42:49 -0500

    New TwythonTimeoutError exception controls retries and sleep time.

--- bot.py
+++ bot.py
@@ -6,6 +6,12 @@ from datetime import datetime, timedelta
 from twython import Twython, TwythonError
 from mulder import generate_clean_sentence
 
+class TwythonTimeoutError(TwythonError):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return self.value
+
 class bot:
     def __init__(self, c_key, c_secret, a_token, a_token_secret):
         self.api = Twython(c_key, c_secret, a_token, a_token_secret)
@@ -25,15 +31,15 @@ class bot:
     def tweet_generated_msg(self):
         sentence = generate_clean_sentence(True) # True means Twitter
         print sentence
-        try:
+        try: # Split into generic/specific TwythonError. No contingency plan.
             #self.api.update_status(status = sentence)
             self.autofail(sentence)
-        except TwythonError as err_msg:
-            e = str(err_msg)
+        except TwythonError as err:
+            e = str(err)
             if 'Read timed out' in e:
-                print "TIMEOUT\n    " + e + "\n    Not retrying."
+                raise TwythonTimeoutError(e)
             else:
-                print "FAIL\n    " + e + "\n    Not retrying."
+                raise TwythonError(e)
 
 if __name__ == "__main__":
     keys_tokens = open('keys_tokens.txt', 'r').read().splitlines()
@@ -48,12 +54,25 @@ if __name__ == "__main__":
     twitter = bot(c_key, c_secret, a_token, a_token_secret)
     print "done"
 
+    n_tries = 0
+    max_tries = 2
     while True:
-        print "tweeting..."
-        twitter.tweet_generated_msg()
         avg_hours = 1.0
         sleep_sec = random.expovariate(1.0 / avg_hours) * 60 * 60
-        print "Done.", 
+        print "tweeting..."
+        try:
+            n_tries += 1
+            twitter.tweet_generated_msg()
+        except TwythonTimeoutError as err:
+            print "Try {} of {} failed.\n    [{}]".format(n_tries, max_tries,
+                                                          str(err))
+            if n_tries < max_tries:
+                sleep_sec = 10
+            else:
+                raise TwythonTimeoutError(str(err))
+        else:
+            print "Done.",
+            n_tries = 0
         print ("Next tweet approx " +
                str(datetime.now() + timedelta(seconds=sleep_sec)))
         time.sleep(sleep_sec)
