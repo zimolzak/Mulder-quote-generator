76628c8bbb82d86b89d1e5212332a9f44b4ad559
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-03-04 23:07:47 -0500

    Reorganize Mulder code a lot: new function suitable for export.

--- mulder.py
+++ mulder.py
@@ -7,19 +7,6 @@ verbs = open('verbs.txt', 'r').read().splitlines()
 conjunctions = open('conjunctions.txt', 'r').read().splitlines()
 twitter = False
 
-for arg in sys.argv:
-    if arg == '--count':
-        n = len(nouns)
-        c = len(conjunctions)
-        v = len(verbs)
-        total = ((n ** 6 * v ** 3 * c ** 2)
-                 + (n ** 4 * v ** 2 * c)
-                 + (n ** 2 * v))
-        print '%e possible 1- 2- and 3-clause sentences.' % total
-        quit()
-    if arg == '--twitter':
-        twitter = True
-
 def generate_sentence(nouns, conjunctions, verbs):
     max_clauses = 3
     num_clauses = random.randrange(max_clauses) + 1
@@ -32,12 +19,34 @@ def generate_sentence(nouns, conjunctions, verbs):
             sentence += [", ", random.choice(conjunctions), " "]
     return sentence
 
-i = 0
-while i < Number_Of_Sentences_To_Generate:
-    sentence = ''.join(generate_sentence(nouns, conjunctions, verbs)) + ".\n"
-    sentence = sentence[0].capitalize() + sentence[1:]
-    if twitter:
-        sentence = sentence[:-1] + " #thexfiles\n"
-    if ( (twitter and len(sentence) < 140) or not twitter):
+def generate_clean_sentence(twitter_flag):
+    ending = ".\n"
+    if twitter_flag:
+        ending = ". #thexfiles\n"
+    while True:
+        sentence = ''.join(generate_sentence(nouns, conjunctions, verbs)) + ending
+        sentence = sentence[0].capitalize() + sentence[1:]
+        if not twitter_flag:
+            return sentence
+        if len(sentence) < 140:
+            return sentence
+
+if __name__ == "__main__":
+    for arg in sys.argv:
+        if arg == '--count':
+            n = len(nouns)
+            c = len(conjunctions)
+            v = len(verbs)
+            total = ((n ** 6 * v ** 3 * c ** 2)
+                     + (n ** 4 * v ** 2 * c)
+                     + (n ** 2 * v))
+            print '%e possible 1- 2- and 3-clause sentences.' % total
+            quit()
+        if arg == '--twitter':
+            twitter = True
+
+    i = 0
+    while i < Number_Of_Sentences_To_Generate:
+        sentence = generate_clean_sentence(twitter)
         print sentence
         i += 1
